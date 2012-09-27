5312853a760414905c4fd523fe5841df407fd27b
Cesar Rivas <crivasg@gmail.com>
2012-09-26 21:45:56 -0500

    Se cambio el script para el uso de comando en cadena usando el pipe (|)

--- log.sh
+++ log.sh
@@ -1,10 +1,6 @@
 
 head -n 4 presidente.txt | sed -e "s/,//g" | sed "s/|/,/g" > summary.txt
+head -n 6 presidente.txt | tail -n 1 > headers.txt
+tail -n +7 presidente.txt |sed "s/|/,/g" > dataife.txt
 
-head -n 6 presidente.txt > summary.txt.old
-tail -n 1 summary.txt.old > headers.txt
-
-tail -n +7 presidente.txt > rawdataife.txt
-sed "s/|/,/g" rawdataife.txt > dataife.txt
-
-rm summary.txt.old
+# EOF
