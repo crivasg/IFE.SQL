a6dc6c3372b7132f308463e893b2eaae18ff2ece
Cesar Rivas <crivasg@gmail.com>
2012-09-27 16:11:07 -0500

    se usar replace_commas.sed en el archivo log.sh

--- log.sh
+++ log.sh
@@ -1,6 +1,6 @@
 
-head -n 4 presidente.txt | sed -e "s/,//g" | sed "s/|/,/g" > summary.txt
-head -n 6 presidente.txt | tail -n 1 > headers.txt
-tail -n +7 presidente.txt |sed "s/|/,/g" > dataife.txt
+head -n 4 presidente.txt  | sed -f replace_commas.sed > summary.txt
+head -n 6 presidente.txt  | tail -n 1 > headers.txt
+tail -n +7 presidente.txt | sed -f replace_commas.sed > dataife.txt
 
 # EOF
