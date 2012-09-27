57889804d72d44a1671e3a01534d942876aec59f
Cesar Rivas <crivasg@gmail.com>
2012-09-27 16:18:40 -0500

    se usa replace_commas.sed para obtener los headers de la base de datos

--- log.sh
+++ log.sh
@@ -1,6 +1,6 @@
 
 head -n 4 presidente.txt  | sed -f replace_commas.sed > summary.txt
-head -n 6 presidente.txt  | tail -n 1 > headers.txt
+head -n 6 presidente.txt  | tail -n 1 | sed -f replace_commas.sed > headers.txt
 tail -n +7 presidente.txt | sed -f replace_commas.sed > dataife.txt
 
 # EOF
