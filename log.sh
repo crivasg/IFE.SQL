319bd0dacd30a88440ba4e723ce19baf2b988306
Cesar Rivas <crivasg@gmail.com>
2012-09-27 17:28:58 -0500

    se agregaron los comandos de sqlite a log.sh y summary.sql

--- log.sh
+++ log.sh
@@ -2,8 +2,13 @@
 
 head -n 4 presidente.txt  | sed -f replace_commas.sed > summary.txt
 head -n 6 presidente.txt  | tail -n 1 | sed -f replace_commas.sed > headers.txt
-tail -n +7 presidente.txt | sed -f replace_commas.sed > dataife.txt
+tail -n +7 presidente.txt | sed -f replace_commas.sed > dataife.csv
 awk -f entidades.awk entidades.dat > entidades.sql
 awk -f summary.awk summary.txt > summary.sql
+
+# insert into SQLite
+cat schema.sql | sqlite3 $1
+cat entidades.sql | sqlite3 $1
+cat summary.sql | sqlite3 $1
 
 # EOF
