b0e40e62506b7220fbce9a00215bbdd86422cb33
Cesar Rivas <crivasg@gmail.com>
2012-09-26 21:42:40 -0500

    Los comandos para extraer la informacion de las actas se reemplzaron por una serie de comandos concatenados

--- log.sh
+++ log.sh
@@ -1,7 +1,5 @@
 
-head -n 4 presidente.txt > summary.txt
-sed "s/,//g" summary.txt> summary.txt.old
-sed "s/|/,/g" summary.txt.old > summary.txt
+head -n 4 presidente.txt | sed -e "s/,//g" | sed "s/|/,/g" > summary.txt
 
 head -n 6 presidente.txt > summary.txt.old
 tail -n 1 summary.txt.old > headers.txt
