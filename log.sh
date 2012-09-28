07a574024fbfc5d2945bbcb6292c8f72d25815c8
Cesar Rivas <crivasg@gmail.com>
2012-09-27 21:05:22 -0500

    Se eliminio linea del script

--- log.sh
+++ log.sh
@@ -2,9 +2,7 @@
 
 TMP_FILE='/tmp/ife.sql.tmp'
 
-
 head -n 4 presidente.txt  | sed -f replace_commas.sed > summary.txt
-head -n 6 presidente.txt  | tail -n 1 | sed -f replace_commas.sed > headers.txt
 tail -n +7 presidente.txt | sed -f replace_commas.sed > dataife.csv
 awk -f entidades.awk entidades.dat > entidades.sql
 awk -f summary.awk summary.txt > summary.sql
@@ -15,6 +13,5 @@ cat summary.sql >> $TMP_FILE
 
 # insert into SQLite
 cat $TMP_FILE | sqlite3 $1
-
 
 # EOF
